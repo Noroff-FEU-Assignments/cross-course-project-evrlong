eee42e25ce03cb29ee7971c521162e8c16e90796
evrlong <erilok51657@stud.noroff.no>
2024-05-20 23:17:49 +0200

    Update checkoutBtn.js
    scope issue

--- JS/components/checkoutBtn.js
+++ JS/components/checkoutBtn.js
@@ -1,9 +1,8 @@
-let cart = JSON.parse(localStorage.getItem("itemsInCart")) || [];
-
 export function checkOut(cart) {
   const checkOutBtn = document.querySelector(".button_complete");
 
   checkOutBtn.addEventListener("click", function () {
+    let cart = JSON.parse(localStorage.getItem("itemsInCart")) || [];
     if (cart.length === 0) {
       return;
     } else {
