083f33f128d0c3c465598a71d70707c1f614a2b0
evrlong <127796398+evrlong@users.noreply.github.com>
2024-09-04 19:15:45 +0200

    Candle shop

--- JS/constants.js
+++
@@ -1,3 +0,0 @@
-export const BASE_API_URL = "https://v2.api.noroff.dev/gamehub";
-
-export const GAMES_ENDPOINT = `${BASE_API_URL}/games`;
--- JS/script.js
+++
@@ -1,6 +0,0 @@
-import { countItems } from "./components/updateCartAmount.js";
-import { countItemsMob } from "./components/updateCartAmount.js";
-let cart = JSON.parse(localStorage.getItem("itemsInCart")) || [];
-
-countItems(cart);
-countItemsMob(cart);
